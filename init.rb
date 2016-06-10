1ac2f245c3073c8770f6d4f4a3fbc9c593ea606e
Stephen Davidson <shdavidson90@gmail.com>
2016-06-10 16:25:46 +0530

    init commit

---
+++ init.rb
@@ -0,0 +1,18 @@
+# Redmine sample plugin
+require 'redmine'
+require File.dirname(__FILE__) + '/lib/zendesk'
+
+Redmine::Plugin.register :redmine_zendesk_ticket_updater do
+  name 'Redmine Zendesk Ticket Updater'
+  author 'Stephen Davidson'
+  description 'Updates associated Zendesk tickets when Redmine issues are updated'
+  version '0.0.1'
+  settings :default => {
+      'zendesk_url' => 'http://support.zendesk.com/',
+      'zendesk_username' => 'zendeskuser',
+      'zendesk_password' => 'zendeskpassword',
+      'field' => nil,
+      'redmine_url' => 'https://your.redmine.url/'
+    },
+    :partial => 'settings/zendesk_plugin_settings'
+end
